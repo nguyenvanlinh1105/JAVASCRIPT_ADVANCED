b79f20319ceb09431a98028924b8bab3ad4ba380
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2024-02-03 22:42:32 +0700

    update statements

--- BAI8_CLOSURE/bai8.js
+++ BAI8_CLOSURE/bai8.js
@@ -7,9 +7,33 @@ function createCounter(){
 }
 
 
-
 const counter1 = createCounter();
 console.log(counter1())
 console.log(counter1())
 console.log(counter1())
 console.log(counter1())
+
+
+function createLogger (namespace){
+    function logger(message){
+        console.log(`${namespace} - ${message}`)
+    }
+    return logger
+}
+const info = createLogger('info');
+console.log(info('Start logging in to server, first time '))
+console.log(info('Start logging in to server, 2nd '))
+const err = createLogger('error: ')
+console.log(err('Đây là lỗi của người dùng'))
+
+function createStorage (key){
+    const store = JSON.parse(localStorage.get(key))?? {}
+    const storage ={
+        get(key){
+            return store(key)
+        },
+        set(key,value){
+            store(key)= value;
+        }
+    }
+}
