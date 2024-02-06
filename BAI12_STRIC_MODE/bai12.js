2e4dd234539ea078fabdebacc2aacb6328797842
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2024-02-06 21:08:33 +0700

    upload new files

--- BAI12_STRIC_MODE/bai12.js
+++ BAI12_STRIC_MODE/bai12.js
@@ -1,15 +1,29 @@
-"use strict";
-fullName ="Nguyen Van Linh"
+// "use strict";
+// fullName ="Nguyen Van Linh"
 function testFunc(){
     "use strict";
-    age = 19;
+   var age = 19;
 }
 testFunc()
-console.log(fullName);
-console.log(age)
+// console.log(fullName);
+// console.log(age)
 
 const student ={
-    fullName :'Nguyễn Văn Linh',
+    fullName :'Nguyễn Văn Linh'
+}// thuộc tính không thể ghi đè  writable = false 
 
-}
+student.fullName="Lyly"
+console.log(student)
+
+
+const linh = {}
+Object.defineProperty(linh,'ten',{
+    value :'An chó',
+    writable:false
+})
+console.log(linh)
+
+delete student// không xóa được 
+console.log(student)
+
 
---
+++ BAI14_PRIMITIVE_REFERENCE-TYPE/bai14.md
@@ -0,0 +1,19 @@
+# Lí thuyết
+# 1. value types (primitive data types)
+ - String 
+ - Number
+ - Boolean 
+ - BigInt 
+ - Symbol
+ - undefined 
+ - null
+# 2.reference type
+ - Object 
+ - Array 
+ - Function 
+## Data type with functions
+- Value types 
+- Reference types 
+
+
+
