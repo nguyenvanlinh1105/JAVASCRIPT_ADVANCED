45421e509f58cbfb663e17bb4643aaaa76968e2c
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2024-02-08 21:14:59 +0700

    update statements

--- BAI16_BIND-p2/bai16.js
+++ BAI16_BIND-p2/bai16.js
@@ -1,5 +1,8 @@
 const  $ = document.querySelector.bind(document);
 const  $$ = document.querySelectorAll.bind(document);
 
+// app manager oto 
 
-console.log($('#heading').innerText)
+const app= (()=>{
+    
+})
