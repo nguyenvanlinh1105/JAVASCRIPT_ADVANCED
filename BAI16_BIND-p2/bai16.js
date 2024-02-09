da9137c217c069ea634ac971766d1aca60a4c754
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2024-02-09 15:51:01 +0700

    update statements

--- BAI16_BIND-p2/bai16.js
+++ BAI16_BIND-p2/bai16.js
@@ -1,8 +1,42 @@
-const  $ = document.querySelector.bind(document);
-const  $$ = document.querySelectorAll.bind(document);
+const $= document.querySelector.bind(document);
+const $$= document.querySelectorAll.bind(document);
+const app = (() => {
+    const cars = ['BMW'];
+    const root = $('#root');
+    const input = $('#input');
+    const submit = $('#submit');
 
-// app manager oto 
+    return {
+        add(car) {
+            if (car !== null && car !== '') {
+                cars.push(car);
+                this.render();
+            }
+        },
+        delete(index) {
+            cars.splice(index, 1);
+            this.render();
+        },
+        render() {
+            const html = cars.map(car => {
+                return `<li>Name car: ${car}</li>`;
+            }).join('');
+            root.innerHTML = html;
+        },
+        init() {
+            // handle dom event
+            submit.onclick = () => {
+                const car = input.value.trim();
+                if (car !== '') {
+                    this.add(car);
+                    input.value = '';
+                    input.focus();
+                }
+            };
 
-const app= (()=>{
-    
-})
+            this.render();
+        }
+    };
+})();
+
+app.init();
